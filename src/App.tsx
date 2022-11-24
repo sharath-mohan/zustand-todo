3d4894da9f150f990e4c3349cbc2b9bef52ee44a
sharath-mohan <sharathdesignsofficial@gmail.com>
2022-11-24 17:18:12 +0530

    async handler added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.css";
 
 import AddTodo from "./AddTodo";
 import Todos from "./Todos";
+import { useTodoStore } from "./app/todo/store";
 
 function App() {
+  const fetchTodos = useTodoStore((state) => state.fetchTodos);
+  useEffect(() => {
+    fetchTodos();
+  }, []);
   return (
     <main>
       <header>
--- src/app/todo/store.ts
+++ src/app/todo/store.ts
@@ -10,6 +10,7 @@ interface TodoState {
   addTodo: (todo: ITodo) => void;
   deleteTodo: (id: string) => void;
   toggleStatus: (id: string) => void;
+  fetchTodos: () => void;
 }
 
 export const useTodoStore = create<TodoState>()(
@@ -27,6 +28,19 @@ export const useTodoStore = create<TodoState>()(
               el.id === id ? { ...el, completed: !el.completed } : el
             ),
           })),
+        fetchTodos: async () => {
+          try {
+            const response = await fetch(
+              "https://jsonplaceholder.typicode.com/users/1/todos"
+            );
+            if (response.ok) {
+              const data: ITodo[] = await response.json();
+              set((state) => ({ todos: [...data] }));
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        },
       }),
       { name: "todos" }
     ),
