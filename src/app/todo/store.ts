53af42a16aed9a3f6a9b9d1c7e518e8ef453cbc3
sharath-mohan <sharathdesignsofficial@gmail.com>
2022-11-24 15:19:08 +0530

    middleware added

--- src/app/todo/store.ts
+++ src/app/todo/store.ts
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { devtools, persist } from "zustand/middleware";
 export interface ITodo {
   id: string;
   isCompleted: boolean;
@@ -10,15 +11,25 @@ interface TodoState {
   deleteTodo: (id: string) => void;
   toggleStatus: (id: string) => void;
 }
-export const useTodoStore = create<TodoState>((set) => ({
-  todos: [],
-  addTodo: (todo) => set((state) => ({ todos: state.todos.concat(todo) })),
-  deleteTodo: (id) =>
-    set((state) => ({ todos: state.todos.filter((el) => el.id !== id) })),
-  toggleStatus: (id) =>
-    set((state) => ({
-      todos: state.todos.map((el) =>
-        el.id === id ? { ...el, isCompleted: !el.isCompleted } : el
-      ),
-    })),
-}));
+
+export const useTodoStore = create<TodoState>()(
+  devtools(
+    persist(
+      (set) => ({
+        todos: [],
+        addTodo: (todo) =>
+          set((state) => ({ todos: state.todos.concat(todo) })),
+        deleteTodo: (id) =>
+          set((state) => ({ todos: state.todos.filter((el) => el.id !== id) })),
+        toggleStatus: (id) =>
+          set((state) => ({
+            todos: state.todos.map((el) =>
+              el.id === id ? { ...el, isCompleted: !el.isCompleted } : el
+            ),
+          })),
+      }),
+      { name: "todos" }
+    ),
+    { name: "todos" }
+  )
+);
